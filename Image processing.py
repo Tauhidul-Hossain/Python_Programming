cb048a912173eede188780d8e3d717f5a70fff1b
Tauhidul Hossain <tauhidulhossaintanmoy@gmail.com>
2022-06-28 00:29:04 +0600

    Update Image processing.py

--- Image processing.py
+++ Image processing.py
@@ -3,4 +3,3 @@ from PIL import Image, ImageFilter, JpegPresets
 image = Image.open('./Image/astro.jpg')
 filtered_img = image.filter(ImageFilter.BLUR)
 filtered_img.save("astro_blur.png", 'png')
-fi
